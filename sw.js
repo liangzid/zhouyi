0318b37db9a616d40b8e5d83efeac5a71911c0fb
liangzid <2273067585@qq.com>
2023-07-22 11:46:44 +0800

    test deployment

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './zhouyi_ui.js',
-  './zhouyi_ui_bg.wasm',
+  './eframe_template.js',
+  './eframe_template_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
