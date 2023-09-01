be88f34cf16d833759ded0e204dd4374962fe78c
unknown <2273067585@qq.com>
2023-09-02 07:56:11 +0800

    add fronted-end interface

--- zhouyi_ui/src/communicate.rs
+++ zhouyi_ui/src/communicate.rs
@@ -2,12 +2,7 @@
 
 
 
-
-
-
-
-
-
+// query and return the login state
 pub fn query_login(email:&String, pwd:&String)->(String,String,String,String){
     (
 	"Ok".to_owned(),
@@ -15,4 +10,36 @@ pub fn query_login(email:&String, pwd:&String)->(String,String,String,String){
 	"regular".to_owned(),
 	"activate".to_owned(),
     )
+}
+
+// appended the new history information into clouds
+pub fn merge_history(email:&String,historys:Vec<(
+    HashMap<String,String>,
+    Vec<String>,
+    Vec<String>,
+    String,
+    String,
+    String,
+    String,
+    Vec<(String,String)>,
+)>)->String{
+
+    "Ok"
+    
+}
+
+
+
+// sync the history info from server to local devices(email)
+pub fn get_history(email:&String)->Vec<(
+    HashMap<String,String>,
+    Vec<String>,
+    Vec<String>,
+    String,
+    String,
+    String,
+    String,
+    Vec<(String,String)>,
+)>{
+    vec![]
 }
