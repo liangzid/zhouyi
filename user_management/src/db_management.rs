5d69325b33b899d3ca66a7d8117b4c4525a17666
unknown <2273067585@qq.com>
2023-09-04 16:26:16 +0800

    sea_orm attempts.

--- user_management/src/db_management.rs
+++ user_management/src/db_management.rs
@@ -1,32 +1,78 @@
-use sled;
+// use sled;
+// use async_std;
+use std::collections::{HashMap};
 use sea_orm::entity::prelude::*;
-use sea_orm::{DatabaseConnection,Database};
+use sea_orm::{ConnectionTrait,DbBackend,Statement,DatabaseConnection,Database,DbErr};
+use futures::executor::block_on;
 
-#[derive(Clone,Debug,PartialEq,DeriveEntityModel)]
-#[sea_orm(table_name = "")]
-pub struct Zhouyi_Record {
-    #[sea_orm(primary_key)]
-    pub email:String,
-    pub pwd:String,
-    pub activation_state:String,
-    pub user_type:String,
-    pub history:Vec<(
-	HashMap<String,String>,
-	Vec<String>,
-	Vec<String>,
-	String,
-	String,
-	String,
-	String,
-	Vec<(String,String)>,
-    )>
-}
+mod migrator;
+
+const DB_URL:&str="sqlite:./sqlite.db?mode=rwc";
+const DB_NAME:&str = "zhouyi_overall";
+
+// #[derive(Clone,Debug,PartialEq,DeriveEntityModel)]
+// #[sea_orm(table_name = "zhouyi_record")]
+// pub struct Model {
+//     #[sea_orm(primary_key)]
+//     pub email:String,
+//     pub pwd:String,
+//     pub activation_state:String,
+//     pub user_type:String,
+//     pub history:Vec<(
+// 	HashMap<String,String>,
+// 	Vec<String>,
+// 	Vec<String>,
+// 	String,
+// 	String,
+// 	String,
+// 	String,
+// 	Vec<(String,String)>,
+//     )>
+// }
 
 // initial the database
-pub fn main(){
-    let newdb=sled::open("../db/data.sled").unwrap();
-    let db: DatabaseConnection = Database::connect("sqlite://username:password@host/database").await?;
+// #[async_std::main]
+async fn run()->Result<(),DbErr>{
+    let db: DatabaseConnection = Database::connect
+    (DB_URL).await?;
     
-    println!("{:?}",&newdb);
+    let db = &match db.get_database_backend() {
+               DbBackend::MySql => {
+                   db.execute(Statement::from_string(
+                       db.get_database_backend(),
+                       format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
+                   ))
+                   .await?;
+        
+                   let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+                   Database::connect(&url).await?
+               }
+               DbBackend::Postgres => {
+                   db.execute(Statement::from_string(
+                       db.get_database_backend(),
+                       format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+                   ))
+                   .await?;
+                   db.execute(Statement::from_string(
+                       db.get_database_backend(),
+                       format!("CREATE DATABASE \"{}\";", DB_NAME),
+                   ))
+                   .await?;
+        
+                   let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+                   Database::connect(&url).await?
+               }
+               DbBackend::Sqlite => db,
+           };
+
+    println!("{:?}",&db);
+    Ok(())
     // drop(newdb);
+}
+
+
+fn main(){
+    if let Err(err)=block_on(run()){
+        panic!("{}",err);
+    }
 }
---
+++ user_management/src/migrator/m20230903_000001_create_account_table.rs
@@ -0,0 +1,49 @@
+use sea_orm_migration::prelude::*;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m_20230903_000001_create_account_table"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    // Define how to apply this migration: Create the Bakery table.
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Zhouyi::Table)
+                    .col(
+                        ColumnDef::new(Zhouyi::Email)
+                            .string()
+                            .not_null()
+                            // .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Zhouyi::Pwd).string().not_null())
+                    .col(ColumnDef::new(Zhouyi::ActivationState).string().not_null())
+                    .col(ColumnDef::new(Zhouyi::UserType).string().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    // Define how to rollback this migration: Drop the Bakery table.
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Zhouyi::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+pub enum Zhouyi {
+    Table,
+    Email,
+    Pwd,
+    ActivationState,
+    UserType,
+}
