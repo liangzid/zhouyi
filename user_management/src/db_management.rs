c3b4f0b4f8639bf817aa58394612ec602686d03b
unknown <2273067585@qq.com>
2023-09-05 09:34:39 +0800

    clean and reformat the db mangement files.

--- user_management/src/db_management.rs
+++ user_management/src/db_management.rs
@@ -322,48 +322,6 @@ pub fn push_records(&self,
 
 }
 
-
-// // initial the database
-// // #[async_std::main]
-// async fn run()->Result<(),DbErr>{
-//     let db: DatabaseConnection = Database::connect
-//     (DB_URL).await?;
-    
-//     let db = &match db.get_database_backend() {
-//                DbBackend::MySql => {
-//                    db.execute(Statement::from_string(
-//                        db.get_database_backend(),
-//                        format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
-//                    ))
-//                    .await?;
-        
-//                    let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-//                    Database::connect(&url).await?
-//                }
-//                DbBackend::Postgres => {
-//                    db.execute(Statement::from_string(
-//                        db.get_database_backend(),
-//                        format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
-//                    ))
-//                    .await?;
-//                    db.execute(Statement::from_string(
-//                        db.get_database_backend(),
-//                        format!("CREATE DATABASE \"{}\";", DB_NAME),
-//                    ))
-//                    .await?;
-        
-//                    let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-//                    Database::connect(&url).await?
-//                }
-//                DbBackend::Sqlite => db,
-//            };
-
-//     println!("{:?}",&db);
-//     Ok(())
-//     // drop(newdb);
-// }
-
-
 fn main(){
     // let dbm= DBManagement::new_dbs("test111.db");
     let mut dbm= DBManagement::open_db("test111.db");
