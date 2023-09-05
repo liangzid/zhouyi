d0b5dfed27bc7d344112afcc964cc55edfcb81fb
unknown <2273067585@qq.com>
2023-09-05 09:32:19 +0800

    fnish the database management.

--- user_management/src/db_management.rs
+++ user_management/src/db_management.rs
@@ -3,9 +3,13 @@
 use std::{collections::{HashMap}, hash::Hash};
 use rusqlite::{params,Connection, Result,ToSql,Error, types::ToSqlOutput};
 use rusqlite;
+use std::string::String;
 
 mod container_wrapper;
 use crate::container_wrapper::{DictStrs,Vec2Str,VecStr};
+
+use serde_json;
+use serde::{Deserialize,Serialize};
 
 #[derive(Clone,Debug,PartialEq,)]
 pub struct OriginalModel {
@@ -25,7 +29,7 @@ pub struct OriginalModel {
     )>
 }
 
-
+#[derive(Serialize,Deserialize)]
 pub struct Account {
     pub email:String,
     pub pwd:String,
@@ -33,10 +37,11 @@ pub struct Account {
     pub user_type:String,
 }
 
+#[derive(Serialize,Deserialize,Debug,Default)]
 pub struct EventRecord {
-    pub id: String,
+    // pub id: String,
     pub email:String,
-    pub gua: DictStrs,
+    pub gua: HashMap<String,String>,
     pub yaos:Vec<String>,
     pub yaoxangs:Vec<String>,
     pub inps:String,
@@ -46,6 +51,49 @@ pub struct EventRecord {
     pub comments:Vec<(String,String)>,
 }
 
+impl EventRecord{
+    pub fn toSqlFormat(&self)->(
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+    ){
+        // 1. serialize `gua yaos yaoxangs comments`
+        let newgua:String=serde_json::to_string(&self.gua).unwrap();
+        let newyaos: String=serde_json::to_string(&self.yaos).unwrap();
+        let newyxs: String=serde_json::to_string(&self.yaoxangs).unwrap();
+        let newcomms: String=serde_json::to_string(&self.comments).unwrap();
+
+        (self.email.clone(),newgua,newyaos,newyxs,
+        self.inps.clone(),self.time.clone(),self.place.clone(),
+        self.analysis.clone(),newcomms)
+    }
+
+    pub fn fromSql(data:(
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+    ))->EventRecord{
+        let gua:HashMap<String,String>=serde_json::from_str(&data.1).unwrap();
+        let yaos:Vec<String>=serde_json::from_str(&data.2).unwrap();
+        let yxs:Vec<String>=serde_json::from_str(&data.3).unwrap();
+        let comments:Vec<(String,String)>=serde_json::from_str(&data.8).unwrap();
+        EventRecord { email: data.0, gua: gua, yaos: yaos,
+             yaoxangs: yxs, inps: data.4, time: data.5,
+              place: data.6, analysis: data.7, comments: comments }
+    }
+}
 
 const DB_URL:&str="../zhouyitest1.db";
 const DB_NAME:&str = "zhouyi_overall";
@@ -76,14 +124,14 @@ pub fn new_dbs(dbpath:&str)->DBManagement{
 	"CREATE TABLE {} (
     id INTEGER  PRIMARY KEY,
     email TEXT NOT NULL,
-    gua BLOB NOT NULL,
-    yaos BLOB NOT NULL,
+    gua TEXT NOT NULL,
+    yaos TEXT NOT NULL,
     yaoxangs BLOB NOT NULL,
     inps TEXT NOT NULL,
     time TEXT NOT NULL,
     place TEXT NOT NULL,
     analysis TEXT NOT NULL,
-    comments BLOB NOT NULL
+    comments TEXT NOT NULL
     )","EventRecord"),
 	()
     ).unwrap();
@@ -189,79 +237,59 @@ pub fn verify_login(&self,email:&str,pwd:&str)->(String,String,String,String){
 }
 
 
-pub fn get_records(&self,email:&str)->Vec<(
-    DictStrs,
-    Vec<String>,
-    Vec<String>,
-    String,
-    String,
-    String,
-    String,
-    Vec<(String,String)>,
-)>{
+pub fn get_records(&self,email:&str)->Result<Vec<EventRecord>>{
 
     // 1. query database
     let sql=format!("SELECT gua, yaos, yaoxangs,
-     inps, time, palce, analysis, comments
+     inps, time, place, analysis, comments
      FROM EventRecord
      WHERE email='{}'",email);
+    //  let sql=format!("SELECT gua
+    //  FROM EventRecord
+    //  WHERE email='{}'",email);
+
     let mut stmt=self.conn.prepare(&sql).unwrap();
     
-    let mut all_datas: Vec<(DictStrs,
-         Vec<String>, 
-         Vec<String>,
-        String,
-        String,
-        String,
-        String,
-        Vec<(String, String)>)>=vec![];
+    let mut all_datas: Vec<EventRecord>=vec![];
     let mut rows=stmt.query([]).unwrap();
-
-    while let record = rows.next().unwrap().expect(""){
-        all_datas.push((
+    while let Some(record) = rows.next().unwrap(){
+        all_datas.push(
+            EventRecord::fromSql
+        ((
+            email.to_owned(),
             record.get(0).unwrap(),
             record.get(1).unwrap(),
             record.get(2).unwrap(),
-
             record.get(3).unwrap(),
             record.get(4).unwrap(),
             record.get(5).unwrap(),
             record.get(6).unwrap(),
-            
             record.get(7).unwrap(),
-        ));
+        )));
     }
-    all_datas
+
+    Ok((all_datas))
 }
 
 // pub fn merge_history(&self, email:&str)
 
 // very straightforward, push one record up.
 pub fn push_records(&self,
-    email:&str,
-    data:(DictStrs, //gua
-    Vec<String>,  //yaos
-    Vec<String>, //yaos' xang
-    String, // inps
-    String, // time
-    String, //place
-    String, //analysis
-    Vec<(String, String)> //comments
-    ))->String{
+    data:EventRecord)->String{
         
         // 1. query database
-        
-        let time=data.4;
+        let email=data.email.clone();
+        let time=data.time.clone();
         
         let sql=format!("SELECT id
         FROM EventRecord
-        WHERE time='{}' AND email='{}'",&time,email);
+        WHERE time='{}' AND email='{}'",&time,&email);
         let mut stmt=self.conn.prepare(&sql).unwrap();
         if stmt.exists([]).unwrap(){
             let item_id:i32=stmt.query([]).unwrap().next()
             .unwrap().expect("").get(0).unwrap();
-
-            let params=params![data.0,
+            let data=EventRecord::toSqlFormat(&data);
+            let params=params![
             data.1,data.2,data.3,data.4,
             data.5,data.6,data.7,data.8,item_id];
             self.conn.execute("UPDATE EventRecord
@@ -278,10 +306,11 @@ pub fn push_records(&self,
             return "Ok".to_owned();
         }
         else{
+            let data=EventRecord::toSqlFormat(&data);
             self.conn.execute("INSERT INTO EventRecord
             (email, gua, yaos, yaoxangs, inps, time, place, analysis, comments)
             VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9)",
-            [email,data.0,data.1,data.2,&data.3,&data.4,&data.5,&data.6,data.7,data.8]
+            [data.0,data.1,data.2,data.3,data.4,data.5,data.6,data.7,data.8]
             );
 
             return "Ok".to_owned();
@@ -343,4 +372,22 @@ fn main(){
     println!("{:?}",res);
     let res=dbm.activate_account("222root@123.com");
     println!("{:?}",res);
+
+    // record management
+    let mut record1=EventRecord::default();
+    record1.email="222root@123.com".to_owned();
+    
+    dbm.push_records(record1);
+
+    let mut record2=EventRecord::default();
+    record2.email="222root@123.com".to_owned();
+    record2.analysis="great".to_owned();
+    record2.time="1111".to_owned();
+    dbm.push_records(record2);
+
+    let res=dbm.get_records("222root@123.com");
+    println!("{:?}",res);
+    
+
+
     }
